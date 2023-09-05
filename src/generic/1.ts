d72e34944ed44536b463d32893ddc367ad22cfb9
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-06 00:34:12 +0300

    1

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -5,15 +5,26 @@
    щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+// function getPromise () {
+//   return new Promise((resolve) => {
+//     resolve(['Text', 50]);
+//   });
+// }
+
+// getPromise ()
+// .then((data) => {
+//   console.log(data);
+// });
+
+// export { };
+
+function getPromise<T>(): Promise<(string | number)[]> {
   return new Promise((resolve) => {
     resolve(['Text', 50]);
   });
 }
 
-getPromise ()
-.then((data) => {
-  console.log(data);
-});
-
-export {};
+getPromise()
+  .then((data) => {
+    console.log(data); 
+  });
