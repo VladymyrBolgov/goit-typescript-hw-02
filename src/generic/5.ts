88bcfda06ff40a81d2a52bf1cf6888ca92c9fccd
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-19 00:41:28 +0300

    upd 5

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,11 +1,19 @@
 /*
   Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення. 
-  Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
+  Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими 
+  типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+// interface KeyValuePair {
+//   key;
+//   value;
+// }
+
+// export {};
+
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
 export {};
